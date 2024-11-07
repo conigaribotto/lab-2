53b0ac08708e577dc98c6f901bbada815c617f29
Scott505 <santysepu@hotmail.com>
2024-11-07 19:46:47 -0300

    Modelo Medico

---
+++ models/Medico.js
@@ -0,0 +1,37 @@
+const { DataTypes } = require('sequelize');
+const sequelize= require('../config/db');
+
+const Medico = sequelize.define('Medico', {
+    id_medico: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    nombre: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    apellido: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    dni: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    id_usuario: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'usuarios',
+            key: 'id_usuario'
+        }
+    }
+}, {
+    tableName: 'medicos',
+    timestamps:false
+});
+
+Medico.belongsTo(Usuario, {foreignKey: 'id_usuario'});
+
+module.exports= Medico;
